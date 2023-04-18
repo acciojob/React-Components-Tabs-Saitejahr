b118bf1abc0062137ff87d37de617beb116882c3
Saitejahr <98202897+Saitejahr@users.noreply.github.com>
2023-04-18 16:55:06 +0530

    react

---
+++ src/components/Tab.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Tab = () => {
+  return (
+    <>
+      <div className="tabs">
+        <div className="tab" id="Home-tab">Home</div>
+        <div className="tab" id="About-tab">About</div>
+        <div className="tab" id="Features-tab">Features</div>
+        <viewport className="viewport">Pages Go Here</viewport>
+      </div>
+    </>
+  )
+}
+
+export default Tab
